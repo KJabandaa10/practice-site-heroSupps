3d3f5557265a0b804d264dba64d0b75fd209c38b
Kartar Jabanda <kartarjabanda@hotmail.co.uk>
2019-08-12 15:43:52 +0100

    add js features - tabs will return to this later

---
+++ app.js
@@ -0,0 +1,10 @@
+const products = document.getElementById("products");
+const productsTab = document.getElementById("products-tab");
+
+function myFunction() {
+  if (products.style.display === "none") {
+    products.style.display = "block";
+  } else {
+    products.style.display = "none";
+  }
+};
